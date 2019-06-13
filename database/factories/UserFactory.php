1bae83ffbb50f678ed35864ea5c0654bd2bd81f7
dev <amadoundiaye709@gmail.com>
2019-06-13 14:12:23 +0000

    Seesers Roles et Villages

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,7 +14,7 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
-
+/* 
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
@@ -23,4 +23,4 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
-});
+}); */
